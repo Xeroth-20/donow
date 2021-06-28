466d2f10eb4b1d41830b7f0d34697ab4d5dcbafc
Jhonatan Castillo <jhonatancastillox@gmail.com>
2021-06-27 23:31:33 -0500

    0.2.5

--- server/index.tsx
+++ server/index.tsx
@@ -5,14 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static('dist'));
-/* app.use(/^(?!\/?api)/, (req, res) => {
-	res.sendFile(path.resolve('dist', 'index.html'), () => {
-		res.status(404).send('Pagina principal no encontrada.').end();
-	});
-}); */
-
-app.get('/', (req, res) => {
-	res.send('Hello').end();
+app.use(/^(?!\/?api)/, (req, res) => {
+	res.sendFile(path.resolve('dist', 'index.html'), () => {});
 });
 
 app.listen(PORT, () => {
